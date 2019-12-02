46a895fcab019db61a9e547253aa660eadcb8075
hfchou <tim_chou@chowsangsang.com>
2019-12-02 17:42:08 +0800

    update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,65 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Footer from "./Footer";
-
+const footerData = {
+  socialmediatitle: "關注我們",
+  socialmediabtns: [
+    {
+      iconclass: "icon-facebook",
+      url: "https://www.facebook.com/emphasis.hk/"
+    },
+    {
+      iconclass: "icon-instagram",
+      url: "https://www.instagram.com/emphasisjewellery/?hl=zh-hk"
+    }
+  ],
+  row1: [
+    {
+      name: "分店位置",
+      url: "/tc/store-locator"
+    },
+    {
+      name:
+        "<a href='/tc/contact-us'>聯絡我們</a> <a href='tel:+85221923228'>+852 2192 3228</a>",
+      url: null
+    },
+    {
+      name: "貨品退換指引",
+      url: "/tc/experience/return-exchange"
+    }
+  ],
+  row2: [
+    {
+      name: "使用條款",
+      url: "//member.emphasis.com/hk/tc/terms-of-use"
+    },
+    {
+      name: "私隱政策",
+      url: "//member.emphasis.com/hk/tc/privacy-policy"
+    },
+    {
+      name: "Cookie政策",
+      url: "//member.emphasis.com/hk/tc/cookie-policy"
+    },
+    {
+      name: "條款及細則",
+      url: "//member.emphasis.com/hk/tc/terms-and-conditions"
+    },
+    {
+      name: "© 2019 周生生集團國際有限公司",
+      url: null
+    },
+    {
+      name: "粤ICP备12061196号-2",
+      url: null
+    }
+  ]
+};
 function App() {
   return (
     <div className="App">
       <div>lorem</div>
-      <Footer />
+      <Footer footerData={footerData} />
     </div>
   );
 }
--- src/Footer.js
+++ src/Footer.js
@@ -1,30 +1,66 @@
 import React, { Component } from "react";
+import "./css/style.css";
 import "./css/footer.css";
-import footerImage from "./img/icon-logo-graphic.svg";
 
 export default class Footer extends Component {
   render() {
+    const {
+      socialmediatitle,
+      socialmediabtns,
+      row1,
+      row2
+    } = this.props.footerData;
+
     return (
       <div id="footer">
         <div className="content">
-          <img className="icon-logo-graohic" src={footerImage}></img>
-          <p className="follow-us">追蹤我們</p>
-          <a
-            class="iconLink"
-            href="https://www.facebook.com/emphasis.hk/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <span class="icon icon-facebook"></span>
-          </a>
-          <a
-            class="iconLink"
-            href="https://www.instagram.com/emphasisjewellery/?hl=zh-hk"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <span class="icon icon-instagram"></span>
-          </a>
+          <div className="icon-logo-graphic"></div>
+          <p className="follow-us">{socialmediatitle}</p>
+          <p className="footer-icons">
+            {socialmediabtns.map((data, index) => (
+              <a
+                className="iconLink"
+                href={data.url}
+                key={index}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className={`icon ${data.iconclass}`} />
+              </a>
+            ))}
+          </p>
+          {/* 第一列 */}
+          <div className="footer-group">
+            {row1 &&
+              row1.map((data, index) => (
+                <div className="footer-group-item" key={index}>
+                  {data.url ? (
+                    <a
+                      href={data.url}
+                      dangerouslySetInnerHTML={{
+                        __html: data.name
+                      }}
+                    />
+                  ) : (
+                    <div
+                      href={data.url}
+                      dangerouslySetInnerHTML={{
+                        __html: data.name
+                      }}
+                    />
+                  )}
+                </div>
+              ))}
+          </div>
+          {/* 第二列 */}
+          <p className="footer-group">
+            {row2 &&
+              row2.map((data, index) => (
+                <div key={index} className={"footer-group-item"}>
+                  {data.url ? <a href={data.url}>{data.name}</a> : data.name}
+                </div>
+              ))}
+          </p>
         </div>
       </div>
     );
